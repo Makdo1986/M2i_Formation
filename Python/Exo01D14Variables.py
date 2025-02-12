e0c04025e1624be4909ceff55da53097748013f1
Matthieu Degraeve <matthieu.degraeve@labom2iformation.fr>
2025-02-12 16:57:55 +0100

    Mardi 12 février 17h

--- Python/Exo01D14Variables.py
+++ Python/Exo01D14Variables.py
@@ -27,3 +27,12 @@ a, b = b, a
 
 print("a = " + str(a) + " / b = " + str(b))
 
+# Récupération du type de variable
+print(type(None)) # None
+print(type(24)) # int
+print(type(24.04)) # float
+print(type("24")) # str
+print(type(True)) # bool
+print(type([1,2,3,4])) # list
+
+# Astuce Cast => Truly value (<> de valeur None, 0, 0.0, False)
